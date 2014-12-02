ab9484d0a4be3b5a1f38576f63d293af6192b631
Conan Brewmaster <conan.brewmaster@gmail.com>
2014-12-02 23:44:15 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 var app = express();
 var filename = 'index.html';
 
+var buffer = fs.readfileSync(filename);
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(Buffer.toString(fs.readFileSync(filename)))
+  response.send(buffer.toString())
 })
 
 app.listen(app.get('port'), function() {
